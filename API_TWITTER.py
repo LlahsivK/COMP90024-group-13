b94701fa52968c181b4ff062875b14322e51ed80
Alexandre <alesaffre@student.unimelb.edu.au>
2021-04-25 18:38:09 +1000

    python scrip update
    Now will stream tweets from Melbourne and will print the status and the location. However often times the location will not be shown

--- API_TWITTER.py
+++ API_TWITTER.py
@@ -5,16 +5,17 @@ import twitter_credentials
 
 class StdOutListener(StreamListener):
 
-    def on_data(self, data):
-        print(data)
+    def on_status(self, status):
+        print(status.text,status.coordinates)
         return True
 
-    def on_error(self, status):
-        print(status)
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
 
 if __name__ == "__main__":
     listener = StdOutListener()
     auth = tweepy.OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
     auth.set_access_token(twitter_credentials.ACCESS_TOKEN_KEY, twitter_credentials.ACCESS_TOKEN_SECRET)
     Stream = Stream(auth, listener)
-    Stream.filter(track=["Melbourne"])
+    Stream.filter(locations=[144.4303,-37.994,145.5839,-37.4095])
